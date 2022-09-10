4536ccdc09b177595804a3bc70ee6b3fb0349845
gliuu <gliuu@users.noreply.github.com>
2022-09-10 11:31:29 -0400

    Add keyboard and audio description support
    Keyboard to match colors of correct, incorrect, misplaced, and unused letters. You can also click on the keys to enter them into guesses in addition to typing.
    
    Option to turn on audio descriptions. Turn this on by right clicking. When this option is on, descriptions of the keys will be read - the letter of the key and whether it's correct, misplaced, incorrect, or unused.
    
    To do: Change screen reader descriptions to audio descriptions, since this isn't really a screen reader. Also add support for a clickable enter button and reading the results out loud.

--- wordle.py
+++ wordle.py
@@ -1,8 +1,9 @@
-import pygame, sys, random
+from pickle import FALSE
+import pygame, sys, random, string
 
 pygame.init()
-pygame.display.set_caption("Wordle Clone")
-WIDTH, HEIGHT = 800, 800
+pygame.display.set_caption("Wordle Clone With Audio Description")
+WIDTH, HEIGHT = 1200, 1200
 SCREEN = pygame.display.set_mode((WIDTH, HEIGHT))
 FPS = 30
 
@@ -10,7 +11,7 @@ GREY = (100, 100, 100)
 DARK_GREY = (20, 20, 20)
 WHITE = (255, 255, 255)
 RED = (255, 108, 108)
-COLOR_INCORRECT = (50, 50, 50)
+COLOR_INCORRECT = (75, 75, 75)
 COLOR_MISPLACED = (255, 193, 53)
 COLOR_CORRECT = (0, 185, 6)
 
@@ -29,9 +30,42 @@ Y_PADDING = 5
 BASE_OFFSET_X = (WIDTH/2)-((NUM_COLS/2)*DX)-((NUM_COLS/2)*RECT_WIDTH)+(((NUM_COLS+1)%2)*(DX/2))
 BASE_OFFSET_Y = (HEIGHT/2)-((NUM_ROWS/2)*DY)-((NUM_ROWS/2)*RECT_HEIGHT)+(((NUM_ROWS+1)%2)*(DY/2))
 
+TOP_ROW = ['q','w','e','r','t','y','u','i','o','p']
+MIDDLE_ROW = ['a','s','d','f','g','h','j','k','l']
+BOTTOM_ROW = ['z','x','c','v','b','n','m']
+
+TOP_ROW_Y = HEIGHT - (65*6) - 20
+MIDDLE_ROW_Y = HEIGHT - (65*5) - 20
+BOTTOM_ROW_Y = HEIGHT - (65*4) - 20
+
+TOP_ROW_X = []
+MIDDLE_ROW_X = []
+BOTTOM_ROW_X = []
+
+for let in range(len(TOP_ROW)):
+    TOP_ROW_X.append((1200-53*len(TOP_ROW))/2 + let*52)
+for let in range(len(MIDDLE_ROW)):
+    MIDDLE_ROW_X.append((1200-53*len(MIDDLE_ROW))/2 + let*52)
+for let in range(len(BOTTOM_ROW)):
+    BOTTOM_ROW_X.append((1200-53*len(BOTTOM_ROW))/2 + let*52)
+
+#Sound effects
+SOUNDS = []
+for let in range(26):
+    link = "audio-alphabet/"
+    link += chr(let+65)
+    link += ".wav"
+    SOUNDS.append(link)
+CORRECT_SOUND = pygame.mixer.Sound("correct.wav")
+MISPLACED_SOUND = pygame.mixer.Sound("misplaced.wav")
+INCORRECT_SOUND = pygame.mixer.Sound("incorrect.wav")
+UNUSED_SOUND = pygame.mixer.Sound("unused.wav")
+SCREENREADER_OFF_SOUND = pygame.mixer.Sound("screenreader_off.wav")
+SCREENREADER_ON_SOUND = pygame.mixer.Sound("screenreader_on.wav")
+
 def main():
     clock = pygame.time.Clock()
-    letter_font = pygame.font.Font(None, 65)
+    letter_font = pygame.font.SysFont(None, 65)
     text = pygame.font.Font(None, 40)
     used_words = []
     curr_word = ""
@@ -42,10 +76,16 @@ def main():
     flag_lose = False
     flag_invalid_word = False
     flag_not_enough_letters = False
+    audio_on = False
     timer_flag_1 = 0
     timer_flag_2 = 0
-    wordlist = [word.replace("\n","") for word in list(open("wordlist.txt"))]
+    incorrect_letters = []
+    yellow_letters = []
+    green_letters = []
+    wordlist = [word.replace("\n","") for word in list(open("sgb-words.txt"))]
     guess_word = random.choice(wordlist)
+    savedLetter = ';'
+    #guess_word = "eerie"
     assert(len(guess_word) == LETTER_LENGTH)
     assert(guess_word.islower())
 
@@ -65,7 +105,7 @@ def main():
                 if event.type == pygame.KEYDOWN:
                     if event.key == pygame.K_BACKSPACE:
                         # Prevents IndexErrors
-                        if curr_word: 
+                        if curr_word:
                             curr_word = curr_word[:-1]
                             curr_letter -= 1
                     elif event.key == pygame.K_RETURN:
@@ -84,15 +124,68 @@ def main():
                     else:
                         if len(curr_word) < LETTER_LENGTH:
                             if event.unicode.isalpha():
+                                if audio_on == True:
+                                    soundObj = pygame.mixer.Sound(SOUNDS[ord(event.unicode.upper())-65])
+                                    soundObj.play()
                                 curr_word += event.unicode.upper()
                                 curr_letter += 1
+                if event.type == pygame.MOUSEBUTTONDOWN:
+                    savedLetter = ';'
+                    if (event.button == 3):
+                        if audio_on == False:
+                            audio_on = True
+                            SCREENREADER_ON_SOUND.play()
+                        else:
+                            audio_on = False
+                            SCREENREADER_OFF_SOUND.play()
+                        continue
+                    if len(curr_word) < LETTER_LENGTH:
+                        if TOP_ROW_X[0] < event.pos[0] < TOP_ROW_X[9] + RECT_WIDTH:
+                            if TOP_ROW_Y < event.pos[1] < TOP_ROW_Y + RECT_HEIGHT:
+                                for let in range(len(TOP_ROW)):
+                                    if TOP_ROW_X[let] < event.pos[0] < TOP_ROW_X[let] + RECT_WIDTH:
+                                        savedLetter = TOP_ROW[let]
+                            elif MIDDLE_ROW_Y < event.pos[1] < MIDDLE_ROW_Y + RECT_HEIGHT:
+                                for let in range(len(MIDDLE_ROW)):
+                                    if MIDDLE_ROW_X[let] < event.pos[0] < MIDDLE_ROW_X[let] + RECT_WIDTH:
+                                        savedLetter = MIDDLE_ROW[let]
+                            elif BOTTOM_ROW_Y < event.pos[1] < BOTTOM_ROW_Y + RECT_HEIGHT:
+                                for let in range(len(BOTTOM_ROW)):
+                                    if BOTTOM_ROW_X[let] < event.pos[0] < BOTTOM_ROW_X[let] + RECT_WIDTH:
+                                        savedLetter = BOTTOM_ROW[let]
+                    if audio_on == True and savedLetter != ';':
+                        soundObj = pygame.mixer.Sound(SOUNDS[ord(savedLetter.upper())-65])
+                        soundObj.play()
+                        played = False
+                        while played == False:
+                            for correct in green_letters:
+                                if correct == savedLetter:
+                                    CORRECT_SOUND.play()
+                                    played = True
+                                    break
+                            for misplaced in yellow_letters:
+                                if misplaced == savedLetter and played == False:
+                                    MISPLACED_SOUND.play()
+                                    played = True
+                                    break
+                            for incorrect in incorrect_letters:
+                                if incorrect == savedLetter and played == False:
+                                    INCORRECT_SOUND.play()
+                                    played = True
+                                    break
+                            if played == False:
+                                UNUSED_SOUND.play()
+                                played = True
+                if event.type == pygame.MOUSEBUTTONUP and savedLetter != ';':
+                    curr_word += savedLetter.upper()
+                    curr_letter += 1
           
         SCREEN.fill(DARK_GREY)
         # Draw title and underline
         draw_title(letter_font)
         # Draws base 5x6 grid for letters
         for y in range(NUM_ROWS):
-            row_rects = []  
+            row_rects = []
             for x in range(NUM_COLS):
                 x_pos = BASE_OFFSET_X+(x*DX)+(x*RECT_WIDTH)
                 y_pos = BASE_OFFSET_Y+(y*DY)+(y*RECT_HEIGHT)
@@ -125,17 +218,22 @@ def main():
         if timer_flag_2 == TEXT_TIMER * FPS:
             flag_not_enough_letters = False
             timer_flag_2 = 0
+        
+        #sound
+        if audio_on == True:
+            text_surface = text.render("Screen reader is on", True, WHITE)
+            SCREEN.blit(text_surface, (20, 20))
 
         if flag_win:
             text_surface = text.render("Correct! Press 'R' to play again", True, WHITE)
             x_pos = BASE_OFFSET_X - (RECT_WIDTH * (NUM_COLS/5))
-            y_pos = BASE_OFFSET_Y + (DY*7) + (RECT_HEIGHT * NUM_ROWS)
-            SCREEN.blit(text_surface, (x_pos, y_pos))
-        if flag_lose:
-            text_surface = text.render("Try again! Press 'R' to play again", True, WHITE)
-            x_pos = BASE_OFFSET_X - (RECT_WIDTH * (NUM_COLS/5))
-            y_pos = BASE_OFFSET_Y + (DY*7) + (RECT_HEIGHT * NUM_ROWS)
-            SCREEN.blit(text_surface, (x_pos, y_pos))
+            #y_pos = BASE_OFFSET_Y + (DY*7) + (RECT_HEIGHT * NUM_ROWS)
+            SCREEN.blit(text_surface, (x_pos, 200))
+        elif flag_lose:
+            text_surface = text.render("Try again! The word was " + guess_word + ". Press 'R' to play again", True, WHITE)
+            #x_pos = BASE_OFFSET_X - (RECT_WIDTH * (NUM_COLS/5))
+            #y_pos = BASE_OFFSET_Y + (DY*7) + (RECT_HEIGHT * NUM_ROWS)
+            SCREEN.blit(text_surface, (250, 200))
 
         # Blits each letter of the current word the user is currently typing.
         # Firstly renders each letter, then blits it on the appropriate rectangle according to which letter it is.
@@ -172,20 +270,80 @@ def main():
                         # Incorrect Letters
                         if cur_past_letter not in remaining_letters:
                             pygame.draw.rect(SCREEN,COLOR_INCORRECT,curr_rect)
+                            if cur_past_letter not in incorrect_letters:
+                                incorrect_letters.append(cur_past_letter)
                         # Letter exists in word, but wrong position.
                         else:
                             pygame.draw.rect(SCREEN,COLOR_MISPLACED,curr_rect)
+                            if cur_past_letter not in yellow_letters and cur_past_letter not in green_letters:
+                                yellow_letters.append(cur_past_letter)
                             remaining_letters[remaining_letters.index(cur_past_letter)] = ""
                         SCREEN.blit(past_letter_surface,(rects[word_index][letter_index][0]+X_PADDING, rects[word_index][letter_index][1]+Y_PADDING))
+                    else:
+                        cur_past_letter = used_words[word_index][letter_index].lower()
+                        if cur_past_letter not in green_letters:
+                                green_letters.append(cur_past_letter)
+                                if cur_past_letter in yellow_letters:
+                                    yellow_letters.remove(cur_past_letter)
                         
                 # Win/lose condition
                 if num_correct == 5:
                     flag_win = True
                 elif len(used_words) == NUM_ROWS:
-                    flag_lose = True  
+                    flag_lose = True
 
+        #make the keyboard
+        for let in range(len(TOP_ROW)):
+            color = DARK_GREY
+            if TOP_ROW[let] in incorrect_letters:
+                color = COLOR_INCORRECT
+                #column = 50
+            if TOP_ROW[let] in green_letters:
+                color = COLOR_CORRECT
+                #column = 150
+            if TOP_ROW[let] in yellow_letters:
+                color = COLOR_MISPLACED
+                #column = 100
+                # Change font size, also figure out why color is not updating
+            letter_surface = letter_font.render(TOP_ROW[let].upper(), True, WHITE)
+            curr_rect = pygame.Rect((TOP_ROW_X[let], TOP_ROW_Y), (RECT_WIDTH, RECT_HEIGHT))
+            pygame.draw.rect(SCREEN,color,curr_rect)
+            SCREEN.blit(letter_surface, (TOP_ROW_X[let] + 8, TOP_ROW_Y + 5))
+        for let in range(len(MIDDLE_ROW)):
+            color = DARK_GREY
+            if MIDDLE_ROW[let] in incorrect_letters:
+                color = COLOR_INCORRECT
+                #column = 50
+            if MIDDLE_ROW[let] in green_letters:
+                color = COLOR_CORRECT
+                #column = 150
+            if MIDDLE_ROW[let] in yellow_letters:
+                color = COLOR_MISPLACED
+                #column = 100
+                # Change font size, also figure out why color is not updating
+            letter_surface = letter_font.render(MIDDLE_ROW[let].upper(), True, WHITE)
+            curr_rect = pygame.Rect((MIDDLE_ROW_X[let], MIDDLE_ROW_Y), (RECT_WIDTH, RECT_HEIGHT))
+            pygame.draw.rect(SCREEN,color,curr_rect)
+            SCREEN.blit(letter_surface, (MIDDLE_ROW_X[let] + 8, MIDDLE_ROW_Y + 5))
+        for let in range(len(BOTTOM_ROW)):
+            color = DARK_GREY
+            if BOTTOM_ROW[let] in incorrect_letters:
+                color = COLOR_INCORRECT
+                #column = 50
+            if BOTTOM_ROW[let] in green_letters:
+                color = COLOR_CORRECT
+                #column = 150
+            if BOTTOM_ROW[let] in yellow_letters:
+                color = COLOR_MISPLACED
+                #column = 100
+                # Change font size, also figure out why color is not updating
+            letter_surface = letter_font.render(BOTTOM_ROW[let].upper(), True, WHITE)
+            curr_rect = pygame.Rect((BOTTOM_ROW_X[let], BOTTOM_ROW_Y), (RECT_WIDTH, RECT_HEIGHT))
+            pygame.draw.rect(SCREEN,color,curr_rect)
+            SCREEN.blit(letter_surface, (BOTTOM_ROW_X[let] + 8, BOTTOM_ROW_Y + 5))
         pygame.display.update()
         clock.tick(FPS)
+        #pygame.time.get_ticks()
 
 def draw_title(font):
     pygame.draw.line(SCREEN, WHITE, (BASE_OFFSET_X-RECT_WIDTH, BASE_OFFSET_Y-RECT_HEIGHT), (BASE_OFFSET_X + (RECT_WIDTH*(NUM_COLS+1)) + (DX*(NUM_COLS-1)), BASE_OFFSET_Y-RECT_HEIGHT), width=1)
